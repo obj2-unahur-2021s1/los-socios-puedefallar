007f5b62f710a03474c5543246bb8cddd62e6538
NoeliaCq <69400198+NoeliaCq@users.noreply.github.com>
2021-05-10 20:35:10 -0300

    primer requerimiento / sin test

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,23 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Pedido(val valor : Float) { }
+
+class Cliente(val animo : Object, val plataBolsillo : Float, val direccion: Object) {
+
+    fun propinaQueDejaPara(pedido : Pedido): Float {
+       val propinaHumor : Float = when {
+           animo.equals(enojado) -> 0F
+           animo.equals(feliz) -> (pedido.valor * 0.25).toFloat()
+           animo.equals(indiferente) -> plataBolsillo
+           else -> pedido.valor
+
+       }
+        return propinaHumor
+   }
+
 }
+
+object enojado {}
+object feliz {}
+object indiferente {}
+object resfriado {}
