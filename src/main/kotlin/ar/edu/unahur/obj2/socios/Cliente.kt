fb73350d2596f81d8cb796a74aa8626b017aa901
rodrigo <rchiapparo@gmail.com>
2021-05-10 21:25:16 -0300

    Tests hechos - Falta optimizar clase cliente

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,23 +1,50 @@
 package ar.edu.unahur.obj2.socios
 
+import java.util.*
+
 class Pedido(val valor : Float) { }
 
-class Cliente(val animo : Object, val plataBolsillo : Float, val direccion: Object) {
+class Cliente(var animo : Animo, val plataBolsillo: Float, var direccion: Direccion) {
+
+    fun cambiarEstadoDeAnimo(animo :Animo){
+        this.animo = animo
+    }
+    fun cambiarDireccion(direccion: Direccion){
+        this.direccion = direccion
+    }
 
     fun propinaQueDejaPara(pedido : Pedido): Float {
        val propinaHumor : Float = when {
-           animo.equals(enojado) -> 0F
-           animo.equals(feliz) -> (pedido.valor * 0.25).toFloat()
-           animo.equals(indiferente) -> plataBolsillo
+           animo.equals(Enojado) -> 0F
+           animo.equals(Feliz) -> (pedido.valor * 0.25).toFloat()
+           animo.equals(Indiferente) -> plataBolsillo
            else -> pedido.valor
 
        }
-        return propinaHumor
+        return when {
+            direccion.equals(LasRosas) -> propinaHumor + 50
+            direccion.equals(LasLauchas) -> propinaHumor / 2
+            // Falta calcular el maximo
+            direccion.equals(BarrioVerde) -> 200F
+            else -> propinaHumor
+        }
    }
 
-}
 
-object enojado {}
-object feliz {}
-object indiferente {}
-object resfriado {}
+
+}
+abstract class Animo {
+
+
+}
+object Enojado : Animo(){}
+object Feliz : Animo() {}
+object Indiferente : Animo(){}
+object Resfriado : Animo(){}
+
+abstract class Direccion(){}
+
+object LasRosas : Direccion() {}
+object LasLauchas : Direccion() {}
+object BarrioVerde : Direccion() {}
+object LasTorres : Direccion() {}
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,31 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
+  val cliente1 = Cliente(Enojado,500F,LasLauchas)
+
+
+  val pedido1 = Pedido(1000F)
   describe("Un/a cliente") {
+    it ("Cuanto deja si esta enojado"){
+      cliente1.propinaQueDejaPara(pedido1).shouldBe(0)
+    }
+    it ("Cuanto deja si esta feliz"){
+      cliente1.cambiarEstadoDeAnimo(Feliz)
+      cliente1.cambiarDireccion(LasTorres)
+      cliente1.propinaQueDejaPara(pedido1).shouldBe(250.0F)
+    }
+    it ("Cuanto deja si esta indiferente"){
+      cliente1.cambiarEstadoDeAnimo(Indiferente)
+      cliente1.cambiarDireccion(LasTorres)
+      cliente1.propinaQueDejaPara(pedido1).shouldBe(500)
+    }
+    it ("Cuanto deja si esta resfriado"){
+      cliente1.cambiarEstadoDeAnimo(Resfriado)
+      cliente1.cambiarDireccion(LasRosas)
+      cliente1.propinaQueDejaPara(pedido1).shouldBe(1050)
+    }
   }
 })
